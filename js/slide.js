acf65d5ab09f04e1806c5ecf77e239fc4c32f54e
mink92 <kangmin782@gmail.com>
2023-12-16 12:39:07 +0900

    portfolio upload

---
+++ js/slide.js
@@ -0,0 +1,22 @@
+$(function () {
+  var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '">' + (index + 1) + "</span>";
+      },
+    },
+  });
+});
